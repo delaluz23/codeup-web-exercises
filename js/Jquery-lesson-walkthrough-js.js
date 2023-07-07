62e075b8363373d535fa651ecf52897c0f0356e4
Jose De La Luz <Jdelaluz01@gmail.com>
2023-07-07 14:00:02 -0500

    intro to jquery

---
+++ js/Jquery-lesson-walkthrough-js.js
@@ -0,0 +1,9 @@
+//javascript function to alert when page loads
+    // window.onload = (function (){
+    //     alert("window has loaded")
+    // })
+//jquery function to alert when page loads
+//you can use jQuery or $ to call jQuery
+    $(function (){
+        alert("window has loaded")
+    })
