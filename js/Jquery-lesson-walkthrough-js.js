9dcb1cc367df5645c261389ce8f7d34f5180c410
Jose De La Luz <Jdelaluz01@gmail.com>
2023-07-07 15:38:54 -0500

    jquery excercise and walkthrough

--- js/Jquery-lesson-walkthrough-js.js
+++ js/Jquery-lesson-walkthrough-js.js
@@ -4,6 +4,26 @@
     // })
 //jquery function to alert when page loads
 //you can use jQuery or $ to call jQuery
-    $(function (){
-        alert("window has loaded")
+//     $(function (){
+//         alert("window has loaded")
+//     })
+//targeting "id-container" id jquery
+// $(() =>{
+//     $('#id-container').css('background-color', 'red');
+// })
+//js equivalent
+//     document.querySelector('#id-container').style.backgroundColor = 'red'
+//targeting "title" class with jquery
+// $(() =>{
+//     $('.title').css('color', 'purple');
+// })
+//targeting "title" class with js
+    let tittleElements = document.querySelector('.title');
+    tittleElements.forEach(titleElement => {
+        titleElement.style.color = 'blue';
     })
+//use jQuery to select all li elements
+    $(() => {
+        $('li').css('color', 'pink');
+    })
+    //using jquery to check for a value
