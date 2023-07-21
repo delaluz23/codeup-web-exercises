9088b09007794fa2d0a098bbefa93081940f87f0
Jose De La Luz <Jdelaluz01@gmail.com>
2023-07-21 15:37:52 -0500

    refactored to async and await promises

--- js/promises-lecture-js.js
+++ js/promises-lecture-js.js
@@ -1,49 +1,81 @@
-const getPerson = (id) => {
+// const getPerson = (id) => {
+//
+//     const url = `https://swapi.dev/api/people/${id}/`
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             "content-type": "application/json"
+//         }
+//     }
+//     return fetch(url, options)
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .catch((error) =>{
+//             console.log(error)
+//         })
+// };
+//
+// const getFilm = (url) => {
+//     const urlFilm = url
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             "content-type": "application/json"
+//         }
+//     }
+//     return fetch(urlFilm, options)
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .catch(error => {
+//             console.log(error)
+//         })
+// }
 
-    const url = `https://swapi.dev/api/people/${id}/`
-    const options = {
-        method: 'GET',
-        headers: {
-            "content-type": "application/json"
+//using async
+const getPerson = async (id) => {
+    try {
+        const url = `https://swapi.dev/api/people/${id}/`
+        const options = {
+            method: 'GET',
+            headers: {
+                "content-type": "application/json"
+            }
         }
-    }
-    return fetch(url, options)
-        .then((response) => {
-            return response.json();
-        })
-        .catch((error) =>{
-            console.log(error)
-        })
-};
+        const response = await fetch(url, options);
+        const person = await response.json();
+        return person
+    } catch(error) {
+        console.log(error)
+    }}
 
-const getFilm = (url) => {
-    const urlFilm = url
-    const options = {
-        method: 'GET',
-        headers: {
-            "content-type": "application/json"
+const getFilms = async (id) => {
+    try{
+        const url = `https://swapi.dev/api/people/${id}/`
+        const options = {
+            method: 'GET',
+            headers: {
+                "content-type": "application/json"
+            }
         }
+        const response = await  fetch()
+    }catch (error){
+        console.log(error)
     }
-    return fetch(urlFilm, options)
-        .then((response) => {
-            return response.json();
-        })
-        .catch(error => {
-            console.log(error)
-        })
 }
 
+    //async before parameter in arrow functions!
+    (async () => {
+        const person = await getPerson(1)
+        console.log(person)
+
+        // getPerson(2).then((person) => {
+        //     console.log(person);
+        //     getFilm(person.films[0]).then((film) =>{
+        //         console.log(film)
+        //     });
+        // });
 
 
-
-(() => {
-
-    getPerson(2).then((person) => {
-        console.log(person);
-        getFilm(person.films[0]).then((film) =>{
-            console.log(film)
-        });
-    });
-
-
-})();
+    })();
